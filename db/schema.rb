fdf53afb9b51a9e43b573d4bd33030f67067f7d5
Kelby LU <kelby.lu28@gmail.com>
2017-10-24 14:36:16 -0400

    fixed typo in user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024154335) do
+ActiveRecord::Schema.define(version: 20171024183122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "password_digest", null: false
     t.string "fullname", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20171024154335) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["username"], name: "index_user_on_username"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
---
+++ frontend/pxlgram.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<h1>ayo</h1>, rootEl);
+});
