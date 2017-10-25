b5c949c52917c742314d444efb1a28f7e6cf3c49
Kelby LU <kelby.lu28@gmail.com>
2017-10-24 20:13:03 -0400

    Add App and Root with hashrouter and provider

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h3>pxlgram beta</h3>
+  </div>
+);
+
+export default App;
--- frontend/pxlgram.jsx
+++ frontend/pxlgram.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logout, signup } from './actions/session_actions';
 import configureStore from './store/store';
+import Root from './components/root';
+
+//TEST IMPORT
+import { login, logout, signup } from './actions/session_actions';
+//END
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
-  ReactDOM.render(<h1>ayo</h1>, rootEl);
-
   const store = configureStore();
 
+  ReactDOM.render(<Root store={ store } />, rootEl);
+
+  //FUNCS FOR TESTING//
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.login = login;
   window.logout = logout;
   window.signup = signup;
+  //TESTING END//
 });
