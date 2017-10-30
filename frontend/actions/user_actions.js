a3b91222784e3c5fb087685af5425b58f65c8754
Kelby LU <kelby.lu28@gmail.com>
2017-10-30 10:04:17 -0400

    Add user actions

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,30 @@
+import * as UserApiUtil from '../util/user_api_util';
+
+export const RECEIVE_ALL_USERS = 'RECEIVE_ALL_USERS'
+export const RECEIVE_USER = 'RECEIVE_USER'
+
+export const fetchUsers = users => {
+  return ({
+    type: RECEIVE_ALL_USERS,
+    users
+  })
+}
+
+export const fetchUser = user => {
+  return ({
+    type: RECEIVE_USER,
+    user
+  })
+}
+
+export const getUsers = () => dispatch => (
+  UserApiUtil.getUsers().then(users => dispatch(fetchUsers(users)))
+)
+
+export const getUser = (user) => dispatch => (
+  UserApiUtil.getUser(user).then(user => dispatch(fetchUser(user)))
+)
+
+export const editUser = (user) => dispatch => (
+  UserApiUtil.editUser(user).then(user => dispatch(fetchUser(user)))
+)
