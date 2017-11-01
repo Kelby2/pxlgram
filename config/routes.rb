f3f1d4c6c1b2c639b0c4f6dd27bd5aa1fbf830ff
Kelby LU <kelby.lu28@gmail.com>
2017-10-31 20:22:02 -0400

    Add custom route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-  resources :users, only: [:index, :show, :create, :update]
+  resources :users, only: [:index, :show, :create, :update] do
+    resources :photos, only: [:index, :show, :create, :update, :destroy]
+  end
   resources :photos, only: [:index, :show, :create, :update, :destroy]
   resource :sessions, only: [:create, :destroy]
 end
