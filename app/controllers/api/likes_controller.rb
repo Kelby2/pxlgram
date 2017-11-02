4ce543464e441a3bbbf8c0cc5217f18fbe068844
Kelby LU <kelby.lu28@gmail.com>
2017-11-02 00:50:17 -0400

    Complete likes controller

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,7 +1,31 @@
 class LikesController < ApplicationController
+
   def create
+    @user = current_user
+    @photo = Photo.find(params[:photo_id])
+
+    @like = @user.likes.create(@photo.id)
+    
+    if @like.save
+      render 'api/photos/show'
+    else
+      render json: @like.errors.full_messages, status: 422
+    end
+
   end
 
   def destroy
+
+    #find the specific like through current_users likes?
+    @like = current_user.likes.find(params[:id])
+    @photo = Photo.find(@like.photo_id)
+
+    @like.destroy!
+    render 'api/photos/show'
   end
+
+  def like_params
+    params.require(:like).permit(:photo_id)
+  end
+
 end
