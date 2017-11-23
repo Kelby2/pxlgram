be25020054ce126cadb57fceee93108e70cd39fa
Kelby LU <kelby.lu28@gmail.com>
2017-11-23 14:18:59 -0500

    separate dev and prod tools

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,10 +3,17 @@ import RootReducer from '../reducers/root_reducer';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 const configureStore = (preloadedState = {}) => {
   return createStore(RootReducer,
                     preloadedState,
-                    applyMiddleware(thunk, logger))
+                    applyMiddleware(...middlewares))
 };
 
 export default configureStore;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
 const path = require('path');
+const webpack = require('webpack');
+
+let plugins = [];
+let devPlugins = [];
+
+let prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
@@ -7,6 +28,7 @@ module.exports = {
     path: path.join(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
