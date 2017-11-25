a91dfe624f24cd4cc37e15bdeec7c8c4bea32872
Kelby LU <kelby.lu28@gmail.com>
2017-11-25 02:29:06 -0500

    remove unuse font, style searchbar

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -5,22 +5,37 @@ class Searchbar extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      searchTerm: ""
+    }
+
     this.handleInputChange = this.handleInputChange.bind(this);
     this.searchUsers = debounce(this.props.searchUsers, 1500);
   }
 
   handleInputChange(e) {
+    this.setState( { searchTerm: e.currentTarget.value })
+    this.callSearch(e);
+  }
+
+  callSearch(e) {
     if (e.currentTarget.value.length > 0) {
       this.searchUsers(e.currentTarget.value);
     }
+  }
+
+  handleFocus(e) {
+    e.target.select()
   }
 
   render() {
     return (
       <input
         className="user-search-bar"
-        type="text"
+        type="search"
         placeholder="Search"
+        value={ this.state.searchTerm }
+        onFocus={ this.handleFocus }
         onChange={ this.handleInputChange }
       />
     )
