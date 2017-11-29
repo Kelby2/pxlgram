60fca30a5a2930f8e01a11dc9bd863229c97afc7
Kelby LU <kelby.lu28@gmail.com>
2017-11-29 17:22:13 -0500

    change seed names

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@ User.destroy_all
 
 user0 = User.create!(username: 'friend', password: 'password', fullname: 'guest user', email: 'friend@pxlgram.com')
 user1 = User.create!(username: 'admin', password: 'password', fullname: 'administrator', email: 'admin@pxlgram.com', avatar: File.open('app/assets/images/monkey.jpg'), bio: 'I am the moderator here, thank you for checking out pixelgram.')
-user2 = User.create!(username: 'kelbylu', password: 'password', fullname: 'kelby lu', email: 'user2@pxlgram.com', avatar: File.open('app/assets/images/kelby.jpg'), bio: 'Thanks for stopping by!')
-user3 = User.create!(username: 'taylorswift', password: 'password', fullname: 'taylor swift', email: 'user3@pxlgram.com', avatar: File.open('app/assets/images/taylor.jpg'), bio: 'This is the new me. The old me is dead.')
-user3 = User.create!(username: 'beyonce', password: 'password', fullname: 'beyonce knowles', email: 'user31@pxlgram.com', avatar: File.open('app/assets/images/beyonce.jpg'), bio: '#LEmONAdE')
-user4 = User.create!(username: 'jayz', password: 'password', fullname: 'shawn carter', email: 'user5@pxlgram.com', avatar: File.open('app/assets/images/jayz.jpg'), bio: '4:44, check it out, FAM.')
-user5 = User.create!(username: 'pxlg', password: 'password', fullname: 'pxlgram', email: 'user6@pxlgram.com', bio: 'Welcome to pxlgram!', avatar: File.open('app/assets/images/pxlgram_logo.jpg'))
-user6 = User.create!(username: 'nike', password: 'password', fullname: 'just do it', email: 'user7@pxlgram.com', avatar: File.open('app/assets/images/nike.jpg'))
-user7 = User.create!(username: 'jlo', password: 'password', fullname: 'jennifer lopez', email: 'user8@pxlgram.com', avatar: File.open('app/assets/images/jayz.jpg'), bio: 'from around the block.')
-user8 = User.create!(username: 'kevinhart4real', password: 'password', fullname: 'kevin hart', email: 'user9@pxlgram.com', avatar: File.open('app/assets/images/kevinhart.jpg'), bio: 'Hey hey hey, check me out. Live, love & laugh')
-user9 = User.create!(username: 'nba', password: 'password', fullname: 'national basketball association', email: 'user10@pxlgram.com', avatar: File.open('app/assets/images/nike.jpg'))
-user10 = User.create!(username: 'kingjames', password: 'password', fullname: 'the king', email: 'user11@pxlgram.com', avatar: File.open('app/assets/images/cavs.png'), bio: '#23')
-user11 = User.create!(username: 'kobe', password: 'password', fullname: 'kobe bean bryant', email: 'user12@pxlgram.com', avatar: File.open('app/assets/images/lakers-ava.jpg'), bio: "Publisher, Investor, Producer. I produce points.")
+user2 = User.create!(username: 'kelbylu', password: 'password', fullname: 'Kelby Lu', email: 'user2@pxlgram.com', avatar: File.open('app/assets/images/kelby.jpg'), bio: 'Thanks for stopping by!')
+user3 = User.create!(username: 'taylorswift', password: 'password', fullname: 'Taylor Swift', email: 'user3@pxlgram.com', avatar: File.open('app/assets/images/taylor.jpg'), bio: 'This is the new me. The old me is dead.')
+user3 = User.create!(username: 'beyonce', password: 'password', fullname: 'Beyonce Knowles', email: 'user31@pxlgram.com', avatar: File.open('app/assets/images/beyonce.jpg'), bio: '#LEmONAdE')
+user4 = User.create!(username: 'jayz', password: 'password', fullname: 'Shawn Carter', email: 'user5@pxlgram.com', avatar: File.open('app/assets/images/jayz.jpg'), bio: '4:44, check it out, FAM.')
+user5 = User.create!(username: 'pxlg', password: 'password', fullname: 'Pxlgram', email: 'user6@pxlgram.com', bio: 'Welcome to pxlgram!', avatar: File.open('app/assets/images/pxlgram_logo.jpg'))
+user6 = User.create!(username: 'nike', password: 'password', fullname: 'Just Do It', email: 'user7@pxlgram.com', avatar: File.open('app/assets/images/nike.jpg'))
+user7 = User.create!(username: 'jlo', password: 'password', fullname: 'Jennifer Lopez', email: 'user8@pxlgram.com', avatar: File.open('app/assets/images/jayz.jpg'), bio: 'from around the block.')
+user8 = User.create!(username: 'kevinhart4real', password: 'password', fullname: 'Kevin Hart', email: 'user9@pxlgram.com', avatar: File.open('app/assets/images/kevinhart.jpg'), bio: 'Hey hey hey, check me out. Live, love & laugh')
+user9 = User.create!(username: 'nba', password: 'password', fullname: 'National Basketball Association', email: 'user10@pxlgram.com', avatar: File.open('app/assets/images/nike.jpg'))
+user10 = User.create!(username: 'kingjames', password: 'password', fullname: 'Lebron Raymone James', email: 'user11@pxlgram.com', avatar: File.open('app/assets/images/cavs.png'), bio: '#23')
+user11 = User.create!(username: 'kobe', password: 'password', fullname: 'Kobe Bean Bryant', email: 'user12@pxlgram.com', avatar: File.open('app/assets/images/lakers-ava.jpg'), bio: "Publisher, Investor, Producer. I produce points.")
 
 Photo.destroy_all
 
