fb1560ee10eaeba64a727eb8d423d8c54b9b4402
Kelby LU <kelby.lu28@gmail.com>
2017-12-27 16:44:03 -0500

    remove likes reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import CommentsReducer from './comments/comments_reducer';
-import LikeReducer from './likes/likes_reducer';
 import PhotosReducer from './photos/photos_reducer';
 import UsersReducer from './users/users_reducer';
 
