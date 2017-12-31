d1b309274b45235178231b6645ab36ecc0046a16
Kelby LU <kelby.lu28@gmail.com>
2017-12-30 22:17:17 -0500

    add deleteComment function

--- frontend/components/comments/comment_item.jsx
+++ frontend/components/comments/comment_item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { deleteComment } from '../../actions/comment_actions';
 
 class CommentItem extends React.Component {
   constructor(props) {
@@ -11,7 +12,7 @@ class CommentItem extends React.Component {
 
   handleDelete(event) {
     event.preventDefault();
-
+    //this.props.deleteComment(this.props.comment.id)
   }
 
   render() {
