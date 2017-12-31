1ce38af6f6a2f1b8c20374bdc25a60f3ae132564
Kelby LU <kelby.lu28@gmail.com>
2017-12-30 21:52:59 -0500

    set conditional for showing delete button

--- frontend/components/comments/comment_item.jsx
+++ frontend/components/comments/comment_item.jsx
@@ -1,19 +1,72 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const CommentItem = ({ comment }) => {
-  return (
-    <li className='comment'>
-      <Link to={`/${comment.author_name}`}>
-        <span className='comment-author'>{comment.author_name}</span>
-      </Link>
-      <div
-        className="fa fa-times"
-        id="delete-comment-button">
-      </div>
-      <span className='comment-body'>{comment.body}</span>
-    </li>
-  )
+// const CommentItem = ({ comment }) => {
+//   return (
+//     <li className='comment'>
+//       <Link to={`/${comment.author_name}`}>
+//         <span className='comment-author'>{comment.author_name}</span>
+//       </Link>
+//       <div
+//         className="fa fa-times"
+//         id="delete-comment-button">
+//       </div>
+//       <span className='comment-body'>{comment.body}</span>
+//     </li>
+//   )
+// }
+
+class CommentItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);  
+  }
+
+  handleDelete(e) {
+    e.preventDefault();
+    console.log('BAM got deleted!')
+  }
+
+  render() {
+
+    let deleteCommentButton;
+
+    if (this.props.comment.author_name === this.props.currentUser) {
+      deleteCommentButton = (
+        <div
+          className="fa fa-times"
+          id="delete-comment-button"
+          onClick={ this.handleDelete }>
+        </div>
+      )
+    }
+    return (
+      <li className='comment'>
+        <Link to={`/${this.props.comment.author_name}`}>
+          <span className='comment-author'>{this.props.comment.author_name}</span>
+        </Link>
+        { deleteCommentButton }
+        <span className='comment-body'>{this.props.comment.body}</span>
+      </li>
+    )
+  }
 }
 
-export default CommentItem;
+
+const mapStateToProps = state => {
+  const currentUser = state.session.currentUser.username;
+
+  return {
+    currentUser,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return ({
+    deleteComment: id => dispatch(deleteComment(id)),
+  })
+}
+export default connect(mapStateToProps, mapDispatchToProps)
+                        (CommentItem);
