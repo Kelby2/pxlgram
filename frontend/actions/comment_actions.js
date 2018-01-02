d216ed36ed3e44edede0f2e190b62063092a1854
Kelby LU <kelby.lu28@gmail.com>
2018-01-02 16:48:15 -0500

    remove fetchAllComments

--- frontend/actions/comment_actions.js
+++ frontend/actions/comment_actions.js
@@ -1,17 +1,9 @@
 import * as CommentApiUtil from '../util/comment_api_util';
 import { fetchPhoto } from './photo_actions';
 
-export const RECEIVE_ALL_COMMENTS = 'RECEIVE_ALL_COMMENTS';
 export const RECEIVE_PHOTO_COMMENTS = 'RECEIVE_PHOTO_COMMENTS';
 export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
 export const DELETE_COMMENT = 'DELETE_COMMENT';
-
-export const fetchComments = comments => {
-  return ({
-    type: RECEIVE_ALL_COMMENTS,
-    comments
-  })
-}
 
 export const fetchPhotoComments = comments => {
   return ({
@@ -32,11 +24,6 @@ export const removeComment = comment => {
     type: DELETE_COMMENT,
     comment,
   })
-}
-
-export const getComments = () => dispatch => {
-  return CommentApiUtil.getComments()
-      .then(comments => dispatch(fetchComments(comments)))
 }
 
 export const getPhotoComments = photo_id => dispatch => {
