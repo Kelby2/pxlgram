1ed8a1b9a71c09646af4b0ebf7651206184a5dcf
Kelby LU <kelby.lu28@gmail.com>
2018-01-03 02:06:26 -0500

    remove commented code

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,7 +1,7 @@
 import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 export const CLEAR_SESSION_ERRORS = 'CLEAR_SESSION_ERRORS';
 
 const receiveCurrentUser = user => ({
@@ -9,8 +9,8 @@ const receiveCurrentUser = user => ({
     user,
 });
 
-const receiveErrors = errors => ({
-  type: RECEIVE_ERRORS,
+const receiveSessionErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
   errors: errors.responseJSON
 });
 
@@ -26,7 +26,7 @@ export const signup = user => dispatch => {
       dispatch(receiveCurrentUser(user));
     },
     (errors) => {
-      dispatch(receiveErrors(errors));
+      dispatch(receiveSessionErrors(errors));
     });
 };
 
@@ -36,19 +36,15 @@ export const login = user => dispatch => {
       dispatch(receiveCurrentUser(user));
     },
     (errors) => {
-      dispatch(receiveErrors(errors));
+      dispatch(receiveSessionErrors(errors));
     });
 };
 
 export const logout = () => dispatch => {
-  return SessionApiUtil.logout()
-    .then(result => {
-      dispatch(receiveCurrentUser(null));
-    },
-    (errors) => {
-      dispatch(receiveErrors(errors));
-    })
-};
+  return SessionApiUtil.logout().then(result => {
+    dispatch(receiveCurrentUser(null));
+  })
+}
 
 export const clearSessionErrors = () => dispatch => {
   dispatch(removeSessionErrors());
--- frontend/reducers/sessions/session_errors_reducer.js
+++ frontend/reducers/sessions/session_errors_reducer.js
@@ -1,13 +1,15 @@
-import { RECEIVE_ERRORS,
+import { RECEIVE_SESSION_ERRORS,
         RECEIVE_CURRENT_USER,
         CLEAR_SESSION_ERRORS
       } from '../../actions/session_actions';
 
 const SessionErrorsReducer = (initialState = [], action) => {
+  Object.freeze(initialState);
   let newState;
+
   switch (action.type) {
-    case RECEIVE_ERRORS:
-      return action.errors
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
     case RECEIVE_CURRENT_USER:
     case CLEAR_SESSION_ERRORS:
       return [];
