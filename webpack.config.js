0fa77aecf2e509aaaf639a89b2618ae24ce13f6a
Kelby LU <kelby.lu28@gmail.com>
2018-05-23 00:22:53 -0400

    Add updated paperclip gem and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,44 +5,44 @@ var plugins = [];
 const devPlugins = [];
 
 const prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
+	new webpack.DefinePlugin({
+		'process.env': {
+			NODE_ENV: JSON.stringify('production')
+		}
+	}),
+	new webpack.optimize.UglifyJsPlugin({
+		compress: {
+			warnings: true
+		}
+	})
 ];
 
 plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-)
+	process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
-  context: __dirname,
-  entry: path.join(__dirname, "frontend", "pxlgram.jsx"),
-  output: {
-    path: path.join(__dirname, "app", "assets", "javascripts"),
-    filename: "bundle.js"
-  },
-  plugins: plugins,
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env', 'react']
-        }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  }
+	context: __dirname,
+	entry: path.join(__dirname, 'frontend', 'pxlgram.jsx'),
+	output: {
+		path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+		filename: 'bundle.js'
+	},
+	plugins: plugins,
+	module: {
+		loaders: [
+			{
+				test: [/\.jsx?$/, /\.js?$/],
+				exclude: /(node_modules)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env', 'react']
+				}
+			}
+		]
+	},
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.js', '.jsx', '*']
+	}
 };
