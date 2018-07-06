1d06b7a613e86f6a18c40fc5c5aa33209ee7abf8
Kelby Lu <kelby.lu28@gmail.com>
2018-07-06 17:39:13 -0400

    Remove container and refactored landing page code

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util'
 import HeaderContainer from './header/header_container';
-import LandingPageContainer from './landing_page_container';
+import LandingPage from './landing_page';
 import PhotoUploadContainer from './photos/photo_upload_container';
 import UserContainer from './users/user_profile_container';
 import UserEditContainer from './users/edit_user_container';
@@ -19,7 +19,7 @@ const App = () => (
         <ProtectedRoute path='/photos/:photoId' component={ PhotoModalContainer } />
         <ProtectedRoute exact path='/:username/edit' component= { UserEditContainer } />
         <ProtectedRoute exact path='/:username' component={ UserContainer } />
-        <Route exact path='/' component={LandingPageContainer} />
+        <Route exact path='/' component={LandingPage} />
       </Switch>
     </header>
   </div>
--- frontend/components/landing_page.jsx
+++ frontend/components/landing_page.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import SessionPage from './sessions/session_entry_page';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import SessionEntryPage from './sessions/session_entry_page';
 import PhotoIndexContainer from './photos/photo_index_container';
+import { clearSessionErrors } from '../actions/session_actions';
 
-class LandingPage extends React.Component {
-  constructor(props) {
-    super(props)
+const LandingPage = props => {
+  if (props.loggedIn) {
+    return <PhotoIndexContainer />;
   }
 
-  render() {
-    return (this.props.loggedIn) ? <PhotoIndexContainer /> :
-                      <SessionPage clearSessionErrors={ this.props.clearSessionErrors }/>
-  }
-}
+  return (
+    <SessionEntryPage clearSessionErrors={ props.clearSessionErrors } />
+  );
+};
 
-export default LandingPage;
+const mapStateToProps = state => {
+  const loggedIn = state.session.curentUser ? true : false;
+  return { loggedIn };
+};
+
+const mapDispatchToProps = dispatch => {
+  return ({
+    clearSessionErrors: () => dispatch(clearSessionErrors),
+  });
+};
+
+export default withRouter(connect(
+  mapStateToProps, mapDispatchToProps)(LandingPage));
