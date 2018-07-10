168319f696b0a200ece56109be15c5d386183b28
Kelby Lu <kelby.lu28@gmail.com>
2018-07-10 14:07:36 -0400

    Fixed a bug to correctly route users to the correct profile page when clicking on their name in photo modal

--- frontend/components/comments/comment_index.jsx
+++ frontend/components/comments/comment_index.jsx
@@ -21,7 +21,7 @@ class CommentIndex extends React.Component {
       photoCaption = (
         <li className='caption-container'>
           <span className='author-username'>
-            <Link to={`${this.props.photo.author_name}`}>
+            <Link to={`/${this.props.photo.author_name}`}>
               {(this.props.photo.caption && this.props.photo.caption.length > 0) ? this.props.photo.author_name : null}
             </Link>
           </span>
