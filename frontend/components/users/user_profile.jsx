f2047defd7f098d9616e30f9396c808b2f66b1cb
Kelby Lu <kelby.lu28@gmail.com>
2018-07-12 13:46:28 -0400

    Refactored user info out to own component, created state to load all photos at the same time

--- frontend/components/users/user_profile.jsx
+++ frontend/components/users/user_profile.jsx
@@ -1,116 +1,62 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { BeatLoader } from 'react-spinners';
+import UserInfoContainer from './user_info_container';
 import PhotoGridItem from '../photos/photo_grid_item';
-import PhotoModalContainer from '../photos/photo_modal_container';
 
 class UserProfile extends React.Component {
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.match.params.username !== this.props.match.params.username) {
-      this.props.getUser(nextProps.match.params.username);
-      this.props.getUserPhotos(nextProps.match.params.username);
+  state = { loadingPhotos: true };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.username !== this.props.match.params.username) {
+      this.setState({ loadingPhotos: true });
+      this.props.getUserPhotos(this.props.match.params.username).then(() => {
+        this.setState({ loadingPhotos: false });
+      });
     }
   }
 
   componentDidMount() {
-    const { getUser, getUserPhotos, match } = this.props;
+    const { getUserPhotos, match } = this.props;
     const { username } = match.params;
 
-    getUser(username);
-    getUserPhotos(username);
-  }
-
-  renderProfileButton() {
-    //if self, renders a button to edit profile
-    //else, will show a button to follow/unfollow user
-
-    const { isCurrentUser, user } = this.props;
-    if (isCurrentUser) {
-      return (
-        <Link to={`/${user.username}/edit`}>
-          <button
-            className='edit-profile-button'>
-            Edit Profile
-          </button>
-        </Link>
-      );
-    }
-
-    return (
-      <button
-        className='toggle-follow-button'>
-        Follow
-      </button>
-    );
-  }
-
-  renderLogOutButton() {
-    return (
-      <div
-        onClick={() => this.props.logout()}
-        className="fa fa-sign-out fa-lg">
-      </div>
+    getUserPhotos(username).then(() =>
+      {this.setState({ loadingPhotos: false });}
     );
   }
 
   render () {
-    const { user, photos } = this.props;
+    const { photos, username } = this.props;
 
-    if (user && user.photoIds) {
-      return (
-        <main className='user-profile-container'>
-          <Route exact path='/:username/photos/:photoId' component={ PhotoModalContainer }/>
-          <div className='user-profile'>
-            <article className='user-profile-header'>
-              <div className='user-avatar-container'>
-                <div className='user-avatar'>
-                  <img className='avatar' src={user.avatarUrl} />
-                </div>
-              </div>
-
-              <div className='user-info-container'>
-                <div className='info-section-1'>
-                  <span className='user-name'>{user.username}</span>
-                  {this.renderProfileButton()}
-                  {this.props.isCurrentUser && this.renderLogOutButton()}
-                </div>
-
-                <div className='info-section-2'>
-                  <span className='user-stats'>
-                    <div className='stat photo-count'>{user.photoIds.length} </div> posts
-                    <div className='stat follower-count'></div>
-                    <div className='stat following-count'></div>
-                  </span>
-                </div>
-
-                <div className='info-section-3'>
-                  <p className='user-bio'>
-                      <span className='user-full-name'>{user.fullname} </span>  {this.props.user.bio}
-                  </p>
-                </div>
-              </div>
-            </article>
-
+    return (
+      <main className='user-profile-container'>
+        <div className='user-profile'>
+          <UserInfoContainer username={username} />
+          { !this.state.loadingPhotos ?
             <article className='user-photos-container'>
               <ul className='photos-grid-container'>
                 {
                   photos.map(photo => {
-                    return(
+                    return (
                       <PhotoGridItem
-                        key={ photo.id }
-                        photo={ photo }
-                        />
-                      );
+                        key={photo.id}
+                        photo={photo} />
+                    );
                   })
                 }
               </ul>
             </article>
-          </div>
-        </main>
-      );
-    } else {
-      return null;
-    }
+            :
+            <div className='loader'>
+              <BeatLoader
+                size={30}
+                color={'#e2e2e2'}
+                loading={ this.state.photosLoaded } />
+            </div>
+          }
+        </div>
+      </main>
+    );
   }
 
 }
--- frontend/components/users/user_profile_container.jsx
+++ frontend/components/users/user_profile_container.jsx
@@ -1,31 +1,23 @@
 import UserProfile from './user_profile';
 import { connect } from 'react-redux';
-import { logout } from '../../actions/session_actions';
-import { getUser } from '../../actions/user_actions';
 import { getUserPhotos } from '../../actions/photo_actions';
 
 const mapStateToProps = (state, ownProps) => {
   const { username } = ownProps.match.params;
-
-  const user = state.entities.users[username];
+  
   const photos = Object.values(state.entities.photos)
     .filter(photo => photo.author_name === username)
     .sort((photo1, photo2) => photo2.created_at.localeCompare(photo1.created_at));
 
-  const isCurrentUser = username === state.session.currentUser.username;
-
   return ({
-    user,
-    isCurrentUser,
+    username,
     photos: Object.keys(photos).map(id => photos[id])
   });
 };
 
 const mapDispatchToProps = dispatch => {
   return ({
-    getUser: username => dispatch(getUser(username)),
     getUserPhotos: username => dispatch(getUserPhotos(username)),
-    logout: () => dispatch(logout())
   });
 };
 
