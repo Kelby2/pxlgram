47baf92f8366012e36b18769d0e8d9c59fdaa330
Kelby Lu <kelby.lu28@gmail.com>
2018-07-15 17:09:12 -0400

    Remove extra import of logger for dev builds

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -6,7 +6,6 @@ import thunk from 'redux-thunk';
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
-  const { logger } = require('redux-logger');
   middlewares.push(logger);
 }
 
