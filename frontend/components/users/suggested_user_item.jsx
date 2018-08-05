57add18d238113e23f435676fc6cef9858901252
Kelby Lu <kelby.lu28@gmail.com>
2018-08-05 13:12:39 -0400

    Change suggested_user_item to class component that has access to Redux state.
    Allows each item to manage and update the follow status

--- frontend/components/users/suggested_user_item.jsx
+++ frontend/components/users/suggested_user_item.jsx
@@ -1,17 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { followUser, unfollowUser } from '../../actions/follow_actions';
 
-const SuggestedUserItem = ({ user }) => {
-  return (
-    <li>
-      <div className='results'>
-        <img className='result-avatar' src={user.avatarUrl} />
-        <aside className='result-user-info'>
-          <div className='result-username'> { user.username } </div>
-          <div className='result-fullname'> { user.fullname }</div>
+class SuggestedUserItem extends React.Component {
+
+  onFollowPress() {
+    if (this.props.currentUserFollows) {
+      this.props.unfollowUser(this.props.user.username);
+    } else {
+      this.props.followUser(this.props.user.username);
+    }
+  }
+
+  render() {
+    const { user } = this.props;
+
+    return (
+      <li id='suggestion-results'>
+        <aside className='suggestion-info'>
+          <Link style={{ marginRight: '16px' }} to={`${user.username}`}>
+            <img className='suggestion-avatar' src={ user.avatarUrl} />
+          </Link>
+          <div className='suggestion-user-info'>
+            <Link to={`${user.username}`}>
+              <div className='result-username'> { user.username } </div>
+            </Link>
+            <div className='result-fullname'> { user.fullname }</div>
+          </div>
         </aside>
-      </div>
-    </li>
-  );
+
+        <button
+          onClick={() => this.onFollowPress()}
+          className={this.props.currentUserFollows ? 'mini-unfollow' : 'mini-follow'}
+          >{this.props.currentUserFollows ? 'Following' : 'Follow' }
+        </button>
+      </li>
+    );
+  }
 };
 
-export default SuggestedUserItem;
+const mapStateToProps = (state, ownProps) => {
+  const currentUser = state.session.currentUser;
+  const currentUserFollows = currentUser.followings.includes(ownProps.user.username);
+
+  return ({ currentUserFollows });
+};
+
+const mapDispatchToProps = dispatch => {
+  return ({
+    followUser: user => dispatch(followUser(user)),
+    unfollowUser: user => dispatch(unfollowUser(user)),
+  });
+};
+
+export default connect(mapStateToProps,
+  mapDispatchToProps)(SuggestedUserItem);
