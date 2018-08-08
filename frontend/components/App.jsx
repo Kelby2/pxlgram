7787f9653e9a3635b705a24333c669f4c29f84ba
Kelby Lu <kelby.lu28@gmail.com>
2018-08-08 12:34:01 -0400

    Remove header navbar from App.jsx, to be placed individually in other files

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ProtectedRoute } from '../util/route_util';
-import HeaderContainer from './header/header_container';
 import LandingPage from './landing_page';
 import PhotoUploadContainer from './photos/photo_upload_container';
 import UserContainer from './users/user_profile_container';
@@ -10,17 +9,14 @@ import PhotoGridContainer from './photos/photo_grid_container';
 
 const App = () => (
   <div id='app'>
-    <ProtectedRoute path='/' component={ HeaderContainer }/>
-    <main id='body'>
-      <Switch>
-        <ProtectedRoute exact path='/upload' component={ PhotoUploadContainer } />
-        <ProtectedRoute exact path='/explore' component={ PhotoGridContainer } />
-        {/* <ProtectedRoute path='/photos/:photoId' component={ PhotoModalContainer } /> */}
-        <ProtectedRoute exact path='/:username/edit' component= { UserEditContainer } />
-        <ProtectedRoute exact path='/:username' component={ UserContainer } />
-        <Route exact path='/' component={LandingPage} />
-      </Switch>
-    </main>
+    <Switch>
+      <ProtectedRoute exact path='/upload' component={ PhotoUploadContainer } />
+      <ProtectedRoute exact path='/explore' component={ PhotoGridContainer } />
+      {/* <ProtectedRoute path='/photos/:photoId' component={ PhotoModalContainer } /> */}
+      <ProtectedRoute exact path='/:username/edit' component= { UserEditContainer } />
+      <ProtectedRoute exact path='/:username' component={ UserContainer } />
+      <Route exact path='/' component={LandingPage} />
+    </Switch>
   </div>
 );
 
